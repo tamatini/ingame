2ee14dabb1ec4206b1802ced6865af8a89d08201
tamatini <tamatini@hotmail.fr>
2020-08-21 12:15:53 -1000

    cleaning lint errors

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -11,7 +11,7 @@ export class LandingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    $(document).ready(function() {
+    $(function() {
       $('.text-animate').css({
         transform: 'translate3d(0, 0, 0)',
         opacity: '1',
